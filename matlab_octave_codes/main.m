7cce21eb10d481a61f10afefd9aab213cda3076e
Fei <enfeizhan@gmail.com>
2014-12-19 15:17:22 +1000

    start finding the best hypothesis

--- matlab_octave_codes/main.m
+++ matlab_octave_codes/main.m
@@ -1,31 +1,17 @@
-%% Machine Learning Online Class - Exercise 4 Neural Network Learning
+%% Machine Learning for Kaggle handwritten digits recognisor competition
 
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     sigmoidGradient.m
-%     randInitializeWeights.m
-%     nnCostFunction.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
 
 %% Initialization
 clear ; close all; clc
 
 %% Setup the parameters you will use for this exercise
-input_layer_size  = 784;  % 20x20 Input Images of Digits
+input_layer_size  = 784;  % 28x28 Input Images of Digits
 hidden_layer_size = 25;   % 25 hidden units
 num_labels = 10;          % 10 labels, from 1 to 10   
                           % (note that we have mapped "0" to label 10)
 
 %% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset. 
+%  We start by first loading and visualizing the dataset. 
 %  You will be working with a dataset that contains handwritten digits.
 %
 
@@ -37,6 +23,7 @@ m = size(dat, 1);
 
 X = dat(:, 2:end);
 y = dat(:,1) + 1; % adapted to 1-based array index
+
 % Randomly select 100 data points to display
 %  sel = randperm(size(X, 1));
 %  sel = sel(1:100);
